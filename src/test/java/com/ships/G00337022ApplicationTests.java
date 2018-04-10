3e1a436a65ff6c16cafa0d9536603ebe8030317c
Cian Gannon <lego@live.ie>
2018-04-10 14:39:32 +0100

    Renamed file

--- src/test/java/com/ships/G00337022ApplicationTests.java
+++ src/test/java/com/ships/G00337022ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GXXXXXXXXApplicationTests {
+public class G00337022ApplicationTests {
 
 	@Test
 	public void contextLoads() {
