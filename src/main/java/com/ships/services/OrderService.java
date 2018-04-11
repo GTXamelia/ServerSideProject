ff18f2e10813e74839bd3d48457f9cd3ab80d0aa
Cian Gannon <lego@live.ie>
2018-04-11 16:55:49 +0100

    Added more order info

--- src/main/java/com/ships/services/OrderService.java
+++ src/main/java/com/ships/services/OrderService.java
@@ -1,10 +1,15 @@
 package com.ships.services;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ships.model.OrderInfo;
+import com.ships.model.Ship;
+import com.ships.model.ShippingCompany;
 import com.ships.repositories.CompanyInterface;
 import com.ships.repositories.OrderInterface;
 import com.ships.repositories.ShipInterface;
@@ -20,6 +25,13 @@ public class OrderService {
 	
 	@Autowired
 	private CompanyInterface companyInt;
+	
+	private Ship ship;
+	
+	private ShippingCompany ShippingCompany;
+	
+	private DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	private Date date = new Date();
 
 	public ArrayList<OrderInfo> getAll() {
 		return (ArrayList<OrderInfo>) orderInt.findAll();
